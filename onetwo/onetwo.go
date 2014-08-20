739c3f990b4ba4566e8c48bf35f831fb740d1f2b
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-08-20 20:16:25 +0100

    factor our FixedXOR

--- onetwo/onetwo.go
+++ onetwo/onetwo.go
@@ -4,19 +4,25 @@ import (
 	"encoding/hex"
 )
 
+func FixedXOR(a []byte, b []byte) []byte {
+	bytes := make([]byte, len(a))
+
+	for i:=0; i<len(a); i++ {
+		bytes[i] = a[i] ^ b[i]
+	}
+
+	return bytes
+}
+
 func Run() {
 	expectedString := "746865206b696420646f6e277420706c6179"
 
-	input, _   := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-	against, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	a, _   := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	b, _ := hex.DecodeString("686974207468652062756c6c277320657965")
 
-	bytes := make([]byte, len(input))
+	result := FixedXOR(a, b)
 
-	for i:=0; i<len(input); i++ {
-		bytes[i] = against[i] ^ input[i]
-	}
-
-	if(hex.EncodeToString(bytes) == expectedString) {
+	if(hex.EncodeToString(result) == expectedString) {
 		print("Hooray!")
 	}
 }

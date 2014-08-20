087d8d1267a0442cde5f232da8604a81e1bb99b4
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-08-20 20:08:20 +0100

    1.2

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/stevebartholomew/matasano-crypto-challenge/onetwo"
+)
+
+func main() {
+	onetwo.Run()
+}
--- oneone/oneone.go
+++ oneone/oneone.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	srcString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-
 	srcBytes, _ := hex.DecodeString(srcString)
 
 	generatedBase64 := base64.StdEncoding.EncodeToString(srcBytes)

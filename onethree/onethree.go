1eb46265eb07d22b079ca33012267198fcb284e9
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-08-20 22:18:05 +0100

    Giving up for the night

--- onethree/onethree.go
+++ onethree/onethree.go
@@ -3,25 +3,19 @@ package onethree
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/stevebartholomew/matasano-crypto-challenge/onetwo"
 )
 
 func Run() {
-	//input, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-	input := []byte("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	input, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
 	for charIndex := 0; charIndex < 256; charIndex++ {
-		char := []byte(fmt.Sprintf("%c ", charIndex))
-
-		results := make([]byte, len(input))
+		keyBytes := make([]byte, len(input))
 
 		for i := 0; i < len(input); i++ {
-			results[i] = input[i] ^ char[0]
+			keyBytes[i] = byte(charIndex)
 		}
 
-		output := make([]byte, len(input))
-
-		hex.Decode(output, results)
-
-		fmt.Println(string(output))
+		fmt.Printf("%s\n", onetwo.FixedXOR(input, keyBytes))
 	}
 }

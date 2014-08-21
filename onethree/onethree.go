82297b3102384ae00977862ac7cb37f008315d4a
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-08-21 17:50:14 +0100

    Dumb english matching

--- onethree/onethree.go
+++ onethree/onethree.go
@@ -3,8 +3,28 @@ package onethree
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
 	"github.com/stevebartholomew/matasano-crypto-challenge/onetwo"
 )
+
+var parts = []string{"ed", "ing", "th", "ey", "ent"}
+
+func Rank(phrase string) int {
+	rank := 0
+
+	words := strings.Split(phrase, " ")
+
+	for _, word := range words {
+		for _, part := range parts {
+			if strings.Contains(word, part) {
+				print(word)
+				rank++
+			}
+		}
+	}
+
+	return rank
+}
 
 func Run() {
 	input, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
@@ -16,6 +36,12 @@ func Run() {
 			keyBytes[i] = byte(charIndex)
 		}
 
-		fmt.Printf("%s\n", onetwo.FixedXOR(input, keyBytes))
+		decodedPhrase := fmt.Sprintf("%s", onetwo.FixedXOR(input, keyBytes))
+
+		rank := Rank(decodedPhrase)
+
+		if rank > 0 {
+			fmt.Println("%s\n", decodedPhrase)
+		}
 	}
 }

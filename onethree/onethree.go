74bc97760d482dba43f61ddc886b4060e7c713e7
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-09-20 22:20:30 +0100

    Complete ranking from dictionary

--- onethree/onethree.go
+++ onethree/onethree.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stevebartholomew/matasano-crypto-challenge/onetwo"
 	"os/exec"
 	"strconv"
+	"regexp"
 )
 
 const DICT_PATH = "/usr/share/dict/words"
@@ -17,12 +18,14 @@ func Rank(phrase string) int {
 
 	words := strings.Split(phrase, " ")
 	for _, word := range words {
-		occurances, err := exec.Command("grep", "-wciF", string(word), DICT_PATH).Output()
+		isAlphaNumeric, _ := regexp.Match("[A-Za-z0-9]+", []byte(word))
 
-		if err == nil {
-			if strconv.Atoi(string(occurances)) == "1" {
-				print("MATCH")
-				rank++
+		if isAlphaNumeric {
+			occurances, _ := exec.Command("grep", "-wciF", string(word), DICT_PATH).Output()
+			i, _ := strconv.Atoi(strings.TrimSpace(string(occurances)))
+
+			if i > 0 {
+				rank += i
 			}
 		}
 	}
@@ -32,6 +35,7 @@ func Rank(phrase string) int {
 
 func Run() {
 	input, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	results := make(map[string]int)
 
 	for charIndex := 0; charIndex < 256; charIndex++ {
 		keyBytes := make([]byte, len(input))
@@ -45,7 +49,19 @@ func Run() {
 		rank := Rank(decodedPhrase)
 
 		if rank > 0 {
-			fmt.Println("%s\n", decodedPhrase)
+			results[decodedPhrase] = rank
 		}
 	}
+
+	topPhrase := ""
+	topRank := 0
+
+	for phrase, rank := range results {
+		if rank > topRank {
+			topPhrase = phrase
+			topRank = rank
+		}
+	}
+
+	fmt.Println(topPhrase)
 }
